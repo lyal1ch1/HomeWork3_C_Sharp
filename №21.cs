729a46bc599590144348b640b2d0fec02d8b760f
lyal1ch1 <106307360+lyal1ch1@users.noreply.github.com>
2022-12-07 17:33:53 +0300

    Add files via upload

---
+++ №21.cs
@@ -0,0 +1,25 @@
+﻿
+//Задача № 21
+Console.WriteLine("Введите координату x1:");
+double x1 = double.Parse (Console.ReadLine()!);
+
+Console.WriteLine("Введите координату y1:");
+double y1 = double.Parse (Console.ReadLine()!);
+
+Console.WriteLine("Введите координату z1:");
+double z1 = double.Parse (Console.ReadLine()!);
+
+Console.WriteLine("Введите координату x2:");
+double x2 = double.Parse (Console.ReadLine()!);
+
+
+Console.WriteLine("Введите координату y2:");
+double y2 = double.Parse (Console.ReadLine()!);
+
+
+Console.WriteLine("Введите координату z2:");
+double z2 = double.Parse (Console.ReadLine()!);
+
+double distance = Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2-y1),2) + Math.Pow((z2-z1),2) );
+
+Console.WriteLine($"Расстояние равно: {distance}" );
---
+++ №23.cs
@@ -0,0 +1,17 @@
+﻿
+//Задача № 23
+
+ Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num < 0)
+{
+    num = -num;
+}
+
+for (int i =1; i < num; i++)
+{
+    Console.Write(Math.Pow(i, 3) + ", ");
+}
+
+Console.Write(Math.Pow(num, 3) + ".");
